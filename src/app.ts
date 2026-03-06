082a65cd01d61927a89b42ab9c67e748d2695bef
RenanSaravalli <renansaravalli@gmail.com>
2026-03-06 18:07:18 -0300

    Separa do app a lógica do cadastro de rotas

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,7 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { prisma } from './lib/prisma.js'
+import { appRoutes } from './http/routes.js'
+
 export const app = fastify()
 
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.email(),
-    password: z.string().min(6),
-  })
-
-  // Faz a validação com o parse, caso não de certo, o código para por aqui
-  const { name, email, password } = registerBodySchema.parse(request.body)
-
-  // Pegamos os dados, agora vamos tentar realizar a criação do usuário
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
-
-  // Retornamos com status 201
-  return reply.status(201).send()
-})
+// Chama as nossas rotas
+app.register(appRoutes)
