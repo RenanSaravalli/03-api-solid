5f0b9294eeb28b7031bc28848e94b6bfbda0cc5c
RenanSaravalli <renansaravalli@gmail.com>
2026-03-08 15:47:47 -0300

    Utilizando o arquivo de service para criar um usuário no nosso arquivo de controller (http)

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import type { FastifyRequest, FastifyReply } from 'fastify'
-import { hash } from 'bcryptjs'
-import { prisma } from '@/lib/prisma.js'
 import z from 'zod'
+import { registerUseCase } from '@/use-cases/register.js'
 
 // controller criar usuário
 
@@ -15,33 +14,16 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   // Faz a validação com o parse, caso não de certo, o código para por aqui
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  // Agora vamos utilizar o método que importamos de bycryptjs, para realizar o hash da senha do usuário.
-  // Vi que o hash retorna uma Promise (temos de usar o await)
-  const password_hash = await hash(password, 6)
-
-  // é legal também antes de cadastrar o usuário, verificar se aquele email já existe vamos retornar um erro mais amigável
-  // Busando usuário pelo método do prisma findUnique (o campo que passarmos deve ser unique ou chave primária)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  // caso exista
-  if (userWithSameEmail) {
-    reply.status(409).send()
-    // mensagem 409 é de conflito
-  }
-
-  // Pegamos os dados, agora vamos tentar realizar a criação do usuário
-  await prisma.user.create({
-    data: {
+  // aqui vamos chamar nosso método de criar o usuário em nossa base, passamos pelo try catch pois damos um erro caso o email do usuário já exista
+  try {
+    await registerUseCase({
       name,
       email,
-      password_hash,
-    },
-  })
+      password,
+    })
+  } catch (err) {
+    reply.status(409).send()
+  }
 
   // Retornamos com status 201
   return reply.status(201).send()
